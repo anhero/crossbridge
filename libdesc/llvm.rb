9c29fcce31f14aa1b975c48ef6789c60d085ca4a
Joe Dupuis <joe.dupuis@anhero.net>
2015-01-18 18:34:47 -0500

    Finished llvm libbuildtool desc

--- libdesc/llvm.rb
+++ libdesc/llvm.rb
@@ -7,13 +7,30 @@ library.installer = Installer::MakeInstall()
 
 library.preparer = make_step do
 	FileUtils.mkdir_p "#{$global_state.work_dir}/llvm-install"
+	FileUtils.mkdir_p "#{$global_state.work_dir}/extra/"
+
 	@library.options.build_dir = @library.work_dir
 	@library.options.CMAKE_DIR = "#{@library.build_subdir}"
+	FileUtils.cp_r "#{$global_state.project_dir}/avm2_env", "#{@library.work_dir}"
 end
 
 
 library.deploy = make_step do
-	raise 'debug'
+	FileUtils.cp_r "#{$global_state.work_dir}/llvm-install/bin/llc#{$build_options.EXEEXT}", "#{$build_options.install_dir}/bin/"
+	FileUtils.cp_r "#{$global_state.work_dir}/llvm-install/bin/llvm-ar#{$build_options.EXEEXT}", "#{$build_options.install_dir}/bin/"
+	FileUtils.cp_r "#{$global_state.work_dir}/llvm-install/bin/llvm-as#{$build_options.EXEEXT}", "#{$build_options.install_dir}/bin/"
+	FileUtils.cp_r "#{$global_state.work_dir}/llvm-install/bin/llvm-diff#{$build_options.EXEEXT}", "#{$build_options.install_dir}/bin/"
+	FileUtils.cp_r "#{$global_state.work_dir}/llvm-install/bin/llvm-dis#{$build_options.EXEEXT}", "#{$build_options.install_dir}/bin/"
+	FileUtils.cp_r "#{$global_state.work_dir}/llvm-install/bin/llvm-extract#{$build_options.EXEEXT}", "#{$build_options.install_dir}/bin/"
+	FileUtils.cp_r "#{$global_state.work_dir}/llvm-install/bin/llvm-ld#{$build_options.EXEEXT}", "#{$build_options.install_dir}/bin/"
+	FileUtils.cp_r "#{$global_state.work_dir}/llvm-install/bin/llvm-link#{$build_options.EXEEXT}", "#{$build_options.install_dir}/bin/"
+	FileUtils.cp_r "#{$global_state.work_dir}/llvm-install/bin/llvm-nm#{$build_options.EXEEXT}", "#{$build_options.install_dir}/bin/"
+	FileUtils.cp_r "#{$global_state.work_dir}/llvm-install/bin/llvm-ranlib#{$build_options.EXEEXT}", "#{$build_options.install_dir}/bin/"
+	FileUtils.cp_r "#{$global_state.work_dir}/llvm-install/bin/opt#{$build_options.EXEEXT}", "#{$build_options.install_dir}/bin/"
+	FileUtils.cp_r Dir["#{$global_state.work_dir}/llvm-install/lib/LLVMgold.*"], "#{$build_options.install_dir}/lib/"
+
+	FileUtils.cp_r "#{@library.work_dir}/bin/fpcmp#{$build_options.EXEEXT}", "#{$global_state.work_dir}/extra/"
+
 end
 
 library.options.CMAKE = "#{library.options.install_dir}/bin/cmake"
@@ -28,35 +45,3 @@ library.options.cmake_options << '-DLLVM_INCLUDE_EXAMPLES=OFF'
 
 library.options.install_dir = "#{$global_state.work_dir}/llvm-install"
 
-
-
-
-# llvm:
-# 		rm -rf $(BUILD)/llvm-debug
-# mkdir -p $(BUILD)/llvm-debug
-
-# cp $(LLVMINSTALLPREFIX)/llvm-install/bin/llc$(EXEEXT) $(SDK)/usr/bin/llc$(EXEEXT)
-# ifeq ($(LLVM_ONLYLLC), false)
-# cp $(LLVMINSTALLPREFIX)/llvm-install/bin/llvm-ar$(EXEEXT) $(SDK)/usr/bin/llvm-ar$(EXEEXT)
-# cp $(LLVMINSTALLPREFIX)/llvm-install/bin/llvm-as$(EXEEXT) $(SDK)/usr/bin/llvm-as$(EXEEXT)
-# cp $(LLVMINSTALLPREFIX)/llvm-install/bin/llvm-diff$(EXEEXT) $(SDK)/usr/bin/llvm-diff$(EXEEXT)
-# cp $(LLVMINSTALLPREFIX)/llvm-install/bin/llvm-dis$(EXEEXT) $(SDK)/usr/bin/llvm-dis$(EXEEXT)
-# cp $(LLVMINSTALLPREFIX)/llvm-install/bin/llvm-extract$(EXEEXT) $(SDK)/usr/bin/llvm-extract$(EXEEXT)
-# cp $(LLVMINSTALLPREFIX)/llvm-install/bin/llvm-ld$(EXEEXT) $(SDK)/usr/bin/llvm-ld$(EXEEXT)
-# cp $(LLVMINSTALLPREFIX)/llvm-install/bin/llvm-link$(EXEEXT) $(SDK)/usr/bin/llvm-link$(EXEEXT)
-# cp $(LLVMINSTALLPREFIX)/llvm-install/bin/llvm-nm$(EXEEXT) $(SDK)/usr/bin/llvm-nm$(EXEEXT)
-# cp $(LLVMINSTALLPREFIX)/llvm-install/bin/llvm-ranlib$(EXEEXT) $(SDK)/usr/bin/llvm-ranlib$(EXEEXT)
-# cp $(LLVMINSTALLPREFIX)/llvm-install/bin/opt$(EXEEXT) $(SDK)/usr/bin/opt$(EXEEXT)
-# cp $(LLVMINSTALLPREFIX)/llvm-install/lib/LLVMgold.* $(SDK)/usr/lib/LLVMgold$(SOEXT)
-# cp -f $(BUILD)/llvm-debug/bin/fpcmp$(EXEEXT) $(BUILDROOT)/extra/fpcmp$(EXEEXT)
-# endif
-
-
-
-
-
-# cd $(BUILD)/llvm-debug && LDFLAGS="$(LLVMLDFLAGS)" CFLAGS="$(LLVMCFLAGS)" CXXFLAGS="$(LLVMCXXFLAGS)" $(SDK_CMAKE) -G "Unix Makefiles" \
-# 		$(LLVMCMAKEOPTS) -DCMAKE_INSTALL_PREFIX=$(LLVMINSTALLPREFIX)/llvm-install -DCMAKE_BUILD_TYPE=$(LLVMBUILDTYPE) -DLLVM_BUILD_CLANG=$(CLANG) \
-# 		-DLLVM_ENABLE_ASSERTIONS=$(LLVMASSERTIONS) -DLLVM_BUILD_GOLDPLUGIN=ON -DBINUTILS_INCDIR=$(SRCROOT)/$(DEPENDENCY_BINUTILS)/include \
-# 		-DLLVM_TARGETS_TO_BUILD="$(LLVMTARGETS)" -DLLVM_NATIVE_ARCH="avm2" -DLLVM_INCLUDE_TESTS=$(LLVMTESTS) -DLLVM_INCLUDE_EXAMPLES=OFF \
-# 		$(SRCROOT)/$(DEPENDENCY_LLVM) && $(MAKE) -j$(THREADS) && $(MAKE) install
